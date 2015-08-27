37e420690911bf975c76c314e4052cac1f977ce1
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2015-08-27 14:24:45 +0200

    fix spec

--- spec/SWP/TemplatesSystem/Gimme/Context/ContextSpec.php
+++ spec/SWP/TemplatesSystem/Gimme/Context/ContextSpec.php
@@ -52,15 +52,15 @@ class ContextSpec extends ObjectBehavior
     function it_should_save_and_read_current_page_info()
     {
         $currentPage = [
-            "id" => 1
-            "name" => "About"
-            "type" => 1
-            "slug" => "about-us"
-            "templateName" => "static.html.twig"
-            "externalUrl" => null
-            "contentPath" => "/content/about-us"
-            "articles" => null
-            "route_name" => "swp_page_about"
+            "id" => 1,
+            "name" => "About",
+            "type" => 1,
+            "slug" => "about-us",
+            "templateName" => "static.html.twig",
+            "externalUrl" => null,
+            "contentPath" => "/content/about-us",
+            "articles" => null,
+            "route_name" => "swp_page_about",
         ];
 
         $this->setCurrentPage($currentPage)->shouldReturn($this);
